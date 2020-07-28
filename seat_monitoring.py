2d5ac997f203da14c5ce5ff213c2e945a8dcc719
BayernMuller <cjjunk29@gmail.com>
2020-07-28 20:03:20 +0900

    displaying seats state

--- seat_monitoring.py
+++ seat_monitoring.py
@@ -1,19 +1,24 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from random import *
 
 class SeatMonitoring(QWidget):
     def __init__(self):
         super().__init__()
         self.resize(800, 480)
-        self.label = QLabel('test', self)
-        self.lists = [[0 for j in range(20)] for i in range(5)]
+        self.lists = [[randrange(0,3) for j in range(10 if i != 2 else 1)] for i in range(5)]
+        self.colors = [QColor('gray'), QColor('green'), QColor('red')]
         print(self.lists)
 
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
-        qp.fillRect(QRect(0, 0, 800, 480), QColor('white'))
+        qp.fillRect(0, 0, 800, 480, QColor('white'))
+        for i, col in enumerate(self.lists):
+            for j, row in enumerate(col):
+                qp.fillRect(j * 50, i * 50, 30, 30, self.colors[row])
+
         qp.end()
         
 
