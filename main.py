4bbd6ed82e34e1111b89fce1f86cc3168d2329e3
my0614 <aqi222@naver.com>
2020-07-31 17:56:39 +0900

    ADD : rescuepage

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from rescue_1 import *
+from ready_rescue import *
+import threading
 
 class Main(QtWidgets.QStackedWidget):
+    pager = pyqtSignal(int)
     def __init__(self):
         super().__init__()
+        self.pager.connect(self.changePageEvent)
         self.resize(800,480)
+        self.addWidget(Ui_ready_rescue(self.pager))
+        self.addWidget(Ui_rescue_1(self.pager))
+        
+    
+    def changePageEvent(self, index):
+        self.setCurrentIndex(index)
+        if index == 1:
+            self.th = threading.Thread(target = self.widget(1).CountTime)
+            self.th.daemon = True
+            self.th.start()
+        else:
+            self.th.join()
+            self.th = None
 
-if _name_ == "_main_":
+
+if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     ui = Main()
     ui.show()
-    ui.setCurrentIndex(2)
+    ui.setCurrentIndex(0)
     app.exec_()
---
+++ ready_rescue.py
@@ -0,0 +1,58 @@
+from PyQt5 import QtGui ,QtCore, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class Ui_ready_rescue(QtWidgets.QWidget):
+    def __init__(self, pager):
+        super().__init__()
+        self.pager = pager
+
+        self.setObjectName("rescue_1")
+        self.resize(800,480)
+        self.label = QtWidgets.QLabel(self)
+        self.label.setGeometry(QtCore.QRect(100,60,600,250)) # label (x,y,width,heigh)
+        self.label2 = QtWidgets.QLabel(self)
+        self.label2.setGeometry(QtCore.QRect(100,210,600,75))
+        font = QtGui.QFont()
+        font.setFamily("Roboto")
+        font.setPointSize(27)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label2.setFont(font)
+        self.label2.setAlignment(QtCore.Qt.AlignCenter)
+        # label
+        self.label2.setObjectName("rescue_1")
+        self.label.setObjectName("rescue_1")
+        self.label.setText("클릭하면 다음페이지로 넘어갑니다. \n \n \n")
+        self.label.setStyleSheet('background:#1C972F; color: #FFFFFF; font-family: Roboto; font-style: normal;font-weight: bold;font-size: 30px;line-height: 35px; ') # stylesheet background color change
+        #취소 버튼
+        pybutton = QtWidgets.QPushButton('취소하기 없음',self)
+        
+        pybutton.resize(250,80)
+        pybutton.move(100,360)
+        pybutton.setStyleSheet("background: #5E6AD6; color : #FFFFFF; font-size: 27px; line-height: 27px; font-weight : bold; ")
+        pybutton2 = QtWidgets.QPushButton(f'요청하기',self)
+        pybutton2.resize(250,80)
+        pybutton2.move(450,360)
+        pybutton2.setStyleSheet("background: #5E6AD6; color : #FFFFFF; font-size: 27px; line-height: 27px; font-weight : bold; ")
+        pybutton2.clicked.connect(self.movePage)
+    
+    def movePage(self, event):
+        self.pager.emit(1)
+
+
+
+        
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    rescueReqestpage = QtWidgets.QWidget()
+    ui = Ui_rescueReqestpage()
+    ui.setupUi(rescue_1)
+    rescueReqestpage.show()
+    sys.exit(app.exec_())
