96f9eac942ce18a37a038da66d050c811c5d2c2b
BayernMuller <cjjunk29@gmail.com>
2020-08-02 15:45:15 +0900

    Added: the widget paints state of seats

--- seat_monitoring.py
+++ seat_monitoring.py
@@ -3,6 +3,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtSvg import *
 from sub_widgets.Button import Button
+from sub_widgets.Label import Label
 
 class SeatMonitoring(QWidget):
     def __init__(self, pager=None):
@@ -13,7 +14,7 @@ class SeatMonitoring(QWidget):
         self.bus = QSvgRenderer('res/bus.svg')
         self.lists = [[0 for j in range(7)] for i in range(3)]
         self.setStyleSheet('SeatMonitoring { background-color: white }')
-        self.colors = [QColor(192,192,192), QColor('green'), QColor('red')]
+        self.colors = [QColor(0xC4C4C4), QColor(0xFF0000), QColor(0x3300FF)]
         self.createWidgets()
 
     def createWidgets(self):
@@ -21,18 +22,36 @@ class SeatMonitoring(QWidget):
         self.requestBtn.setGeometry(400,400,200,80)
         self.settingBtn = Button('설정', self)
         self.settingBtn.setGeometry(601,400,200,80)
+    
 
     def paintEvent(self, event):
         qp = QPainter(self)
+        self.bus.render(qp, QRectF(33, 95, 734, 290))
+        #draw bus
+
+        count = [0,0,0]
         cX = 161
         cY = [141, 233, 289]
-        self.bus.render(qp, QRectF(33, 95, 734, 290))
         for i, col in enumerate(self.lists):
             for j, row in enumerate(col):
                 rect = QRect(cX + j * 87, cY[i], 50, 50)
                 qp.fillRect(rect, self.colors[row])
+                # draw seats
+                count[row] = count[row] + 1
+                # count seats
+
         qp.drawLine(0, 399, 800, 399)
         qp.drawLine(600, 399, 600, 480)
+        qp.drawLine(399, 400, 399, 480)
+        # draw lines
+
+        qp.setFont(QFont('나눔스퀘어', 13))
+        state = ['미착석 : ', '미착용 : ', '착용중 : ']
+        for i in range(3):
+            qp.fillRect(20 + i * 120, 430, 20, 20, self.colors[i])
+            qp.drawText(50 + i * 120, 445, state[i] + str(count[i]))
+            # draw state
+            
         qp.end()
     
 
---
+++ sub_widgets/Label.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtCore import Qt
+
+class Label(QLabel):
+    def __init__(self, text, parent=None, size=15, color='black'):
+        super().__init__(text, parent)
+        self.setAttribute(Qt.WA_StyledBackground, True)
+        self.setStyleSheet('Label { background-color: #00000000;font: 75 %dpt "나눔스퀘어"; color: %s}' % size, color)
