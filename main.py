11f52217ebae60100b6c01eb9cdfa6d25f3eaff3
my0614 <aqi222@naver.com>
2020-08-03 22:10:16 +0900

    ADD :  modify rescue_1 page background color change

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ class Main(QtWidgets.QStackedWidget):
     pager = pyqtSignal(int)
     def __init__(self):
         super().__init__()
-        self.setStyleSheet("background-color : grb(10,50,100);")
         self.pager.connect(self.changePageEvent) #pager 연결되면 함수불러오기
         self.resize(800,480)
         self.addWidget(Ui_ready_rescue(self.pager)) # index 0
         self.addWidget(Ui_rescue_1(self.pager)) #index 1
         self.addWidget(Ui_rescue_finish(self.pager)) #index 2
 
+    
     
     def changePageEvent(self, index): #changeEvent
         self.setCurrentIndex(index) # 페이지 이동할 index로 페이지 이동
@@ -26,9 +26,11 @@ class Main(QtWidgets.QStackedWidget):
             self.th = threading.Thread(target = self.widget(1).CountTime) #thread 위젯1일때 계속 실행
             self.th.daemon = True
             self.th.start() # thread 시작
+            #self.setBackground(0, QtGui.QBrush(QtGui.QColor(255, 0, 0, 0)))
             
         else:
             self.th.join()
+            #self.setStyleSheet("background-color:none;")
 
 
 if __name__ == "__main__":
--- rescue_1.py
+++ rescue_1.py
@@ -10,11 +10,16 @@ class Ui_rescue_1(QtWidgets.QWidget):
         self.time = 20 # tiem 초기화
         self.g_Flag = True # 기본으로 thread 켜기
         
+        self.mainFrame = QtWidgets.QFrame(self)
+        self.mainFrame.setGeometry(QtCore.QRect(0,0,800,480)) # label (x,y,width,heigh)
+
+
         self.setObjectName("rescue_1")
+        
         self.resize(800,480)
-        self.label = QtWidgets.QLabel(self)
+        self.label = QtWidgets.QLabel(self.mainFrame)
         self.label.setGeometry(QtCore.QRect(100,60,600,250)) # label (x,y,width,heigh)
-        self.label2 = QtWidgets.QLabel(self)
+        self.label2 = QtWidgets.QLabel(self.mainFrame)
         self.label2.setGeometry(QtCore.QRect(100,210,600,75))
         font = QtGui.QFont()
         font.setFamily("Roboto")
@@ -25,6 +30,7 @@ class Ui_rescue_1(QtWidgets.QWidget):
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label2.setFont(font)
         self.label2.setAlignment(QtCore.Qt.AlignCenter)
+        
         # label
         self.label2.setObjectName("rescue_1")
         self.label.setObjectName("rescue_1")
@@ -34,18 +40,18 @@ class Ui_rescue_1(QtWidgets.QWidget):
         self.label2.setStyleSheet('background:#1C972F; color: #FFFFFF; font-family: Roboto; font-style: normal;font-weight: bold;font-size: 27px;line-height: 35px; ') # stylesheet background color change
 
         #취소 버튼
-        self.pybutton = QtWidgets.QPushButton('취소',self)
+        self.pybutton = QtWidgets.QPushButton('취소',self.mainFrame)
         self.pybutton.resize(250,80)
         self.pybutton.move(100,360)
         self.pybutton.setStyleSheet("background: #5E6AD6; color : #FFFFFF; font-size: 27px; line-height: 27px; font-weight : bold; ")
         self.pybutton.clicked.connect(lambda x: self.movePage(0))
 
-        self.pybutton2 = QtWidgets.QPushButton(f'요청..20',self) # 요청 버튼
+        self.pybutton2 = QtWidgets.QPushButton(f'요청..20',self.mainFrame) # 요청 버튼
         self.pybutton2.resize(250,80)
         self.pybutton2.move(450,360)
         self.pybutton2.setStyleSheet("background: #5E6AD6; color : #FFFFFF; font-size: 27px; line-height: 27px; font-weight : bold; ")
         self.pybutton2.clicked.connect(lambda x: self.movePage(2))
-
+        self.mainFrame.setStyleSheet("background-color:rgba(255,0,0,100%);") # 색깔 투명도
 
 
     def movePage(self, index):
@@ -60,8 +66,11 @@ class Ui_rescue_1(QtWidgets.QWidget):
         while self.time > 0 and self.g_Flag: # tiem이 0보다 크면서 g_Flag가 Treu 이면
             self.time -= 1 # tiem 1씩 감소
             self.pybutton2.setText(f"요청..{self.time}") # 버튼 텍스트 바꾸기
-        
             sleep(1)  # 1초간 딜레이
+            if self.time % 2 == 0:
+                self.mainFrame.setStyleSheet("background-color:rgba(255,0,0,100%);") # 색깔 투명도
+            else:
+                self.mainFrame.setStyleSheet("background-color:rgba(255,0,0,50%);") # 색깔 투명도
         if self.time <= 0:
             self.movePage(2) # 다음 페이지로 이동
             
